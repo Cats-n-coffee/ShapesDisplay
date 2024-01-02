f64a1e01639331d18c8118d39e2aec043f82a8d9
Cats-n-coffee <lucie.macaigne@gmail.com>
2024-01-01 19:52:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Silk.NET.Windowing;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+
+namespace ShapesDisplay
+{
+    class Program
+    {
+        private static IWindow _window;
+        static void Main(string[] args)
+        {
+            WindowOptions options = WindowOptions.Default with
+            {
+                Size = new Vector2D<int>(800, 600),
+                Title = "Shapes Display in C#",
+            };
+
+            _window = Window.Create(options);
+            _window.Run();
+        }
+    }
+}
