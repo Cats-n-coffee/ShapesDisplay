b1d7a94c22044ad6129ddf9b538e8db1b0cf0b6b
Cats-n-coffee <lucie.macaigne@gmail.com>
2024-01-01 23:11:38 -0500

    add window and keydown event for closing using the Silk tutorial

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace ShapesDisplay
 {
     class Program
     {
-        private static IWindow _window;
-        static void Main(string[] args)
+        private static IWindow? _window;
+        public static void Main()
         {
             WindowOptions options = WindowOptions.Default with
             {
@@ -16,7 +16,31 @@ namespace ShapesDisplay
             };
 
             _window = Window.Create(options);
+
+            _window.Load += OnLoad;
+            _window.Update += OnUpdate;
+            _window.Render += OnRender;
+
             _window.Run();
+        }
+
+        private static void OnLoad() 
+        {
+            IInputContext input = _window.CreateInput();
+            
+            for ( int i = 0; i < input.Keyboards.Count; i++)
+            {
+                input.Keyboards[i].KeyDown += KeyDown;
+            }
+        }
+
+        private static void OnUpdate(double deltaTime) { Console.WriteLine("Update"); }
+
+        private static void OnRender(double deltaTime) { Console.WriteLine("Render"); }
+
+        private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+        {
+            if (key == Key.Escape) _window.Close();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ShapesDisplay
+
+## References
+https://dotnet.github.io/Silk.NET/docs/opengl/c1/1-hello-window.html
